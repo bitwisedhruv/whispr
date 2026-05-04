a9c5cf38cdbf57dd35db6b9c02bd90d63c008692
Dhruv <monkcode463@gmail.com>
2026-05-04 11:00:49 +0530

    feat: Refined Domain Reliability feature

--- test/features/domain_reliability_test.dart
+++ test/features/domain_reliability_test.dart
@@ -74,7 +74,8 @@ void main() {
         ),
       );
 
-      final service = DomainService(dio: dio, apiKey: 'test_api_key', tavilyApiKey: '');
+      final service =
+          DomainService(dio: dio, apiKey: 'test_api_key', tavilyApiKey: '');
       final report = await service.analyzeDomain('https://safe-domain.com');
 
       expect(report.url, 'https://safe-domain.com');
@@ -107,7 +108,8 @@ void main() {
         ));
       }));
 
-      final service = DomainService(dio: dio, apiKey: 'test_api_key', tavilyApiKey: '');
+      final service =
+          DomainService(dio: dio, apiKey: 'test_api_key', tavilyApiKey: '');
 
       expect(
         () async => await service.analyzeDomain('https://bad-domain.com'),
